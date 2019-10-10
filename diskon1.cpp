44db897f3dd982262301ae386bf31cb9f8acc6ee
Rozziq <38291711+rozziqputra@users.noreply.github.com>
2019-10-10 14:22:01 +0800

    Update diskon1.cpp

--- diskon1.cpp
+++ diskon1.cpp
@@ -1,1 +1,32 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int bayar,akhir,diskon,uang;
+    cout<<"Contoh Program c++ Diskon"<<endl<<"Kasir Balog"<<endl;
+    cout<<"--------------------------"<<endl;
+    cout<<"Total pembayaran : ";
+    cin>>bayar;
+    if ((bayar>=100000) && (bayar<500000)){
+        diskon=bayar*0.10;
+        akhir=bayar-diskon;
+        cout<<endl<<"selamat anda mendapatkan diskon 10%"<<endl;
+    } else if ((bayar>=500000) && (bayar<1000000)){
+        diskon=bayar*0.2;
+        akhir=bayar-diskon;
+        cout<<endl<<"selamat anda mendapatkan diskon 20%"<<endl;
+    } else if (bayar>=1000000){
+        diskon=bayar*0.3;
+        akhir=bayar-diskon;
+        cout<<endl<<"selamat anda mendapatkan diskon 30%"<<endl;
+    }else {
+        akhir=bayar;
+    }
+    cout<<endl<<"jadi total pembayarannya = "<<akhir<<endl;;
+    cout<<"Masukkan jumlah uang : ";
+    cin>>uang;
+    cout<<"-------------------------"<<endl;
+    int kembalian= uang-akhir;
+    cout<<"Kembalian anda : "<<kembalian;
+    return 0;
+}
